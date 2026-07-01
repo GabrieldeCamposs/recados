668629528de785cee694ce688a34053d5a216aab
Gabriel <gabrielcampos220@gmail.com>
2026-07-01 00:25:09 -0300

    fix: remove middleware statefulApi e adiciona react-router-dom

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,12 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
-
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+ 
