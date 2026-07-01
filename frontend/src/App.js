dd3d3c9ae9f153981b0bb71e71b8208e7106baab
Gabriel <gabrielcampos220@gmail.com>
2026-07-01 00:09:51 -0300

    feat: integracao das paginas no App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import LoginPage from './pages/LoginPage';
+import CadastroPage from './pages/CadastroPage';
+import RecadosPage from './pages/RecadosPage';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login"   element={<LoginPage />} />
+          <Route path="/cadastro" element={<CadastroPage />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <RecadosPage />
+              </PrivateRoute>
+            }
+          />
+          {/* qualquer rota desconhecida vai para login */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
-
-export default App;
